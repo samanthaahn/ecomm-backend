850a4467e7f87b7fc170ff8403334277ead08035
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-18 15:24:38 -0700

    added sequalize

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequalize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
