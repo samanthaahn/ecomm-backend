4831a22ecbb4310851c7de52f3e63dfff7c806c1
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-18 15:40:44 -0700

    switched prduct and producttag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -12,29 +12,17 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true,
-      },
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true,
-      },
-    },
-    category_id: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
         key: 'id',
       },
     },
