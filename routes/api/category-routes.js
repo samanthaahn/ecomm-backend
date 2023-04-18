7d5580c484cb9a48e14e1a42bab534ae7b043b7b
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-18 16:49:30 -0700

    finished category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,8 +40,18 @@ res.status(200).json(categoryData)
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.delete('/:id', (req, res) => {
